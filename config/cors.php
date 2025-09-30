f522fad88d77994993d354928322cb8af7c4553f
ShivanshRai1 <raishivansh123@gmail.com>
2025-09-30 13:25:39 +0530

    Updated CORS

--- config/cors.php
+++ config/cors.php
@@ -3,12 +3,10 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => [
-        'https://react-frontend-mauve-six.vercel.app',
-    ],
+    'allowed_origins' => ['*'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 ];

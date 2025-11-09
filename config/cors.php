ffaa1616e7bea4eef103e4b3daf1cf80fb135795
ShivanshRai1 <raishivansh123@gmail.com>
2025-11-09 11:19:34 +0530

    simplified cros

--- config/cors.php
+++ config/cors.php
@@ -1,22 +1,22 @@
 <?php
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
     'allowed_methods' => ['*'],
+
     'allowed_origins' => [
-        'http://localhost:3000',
-        'http://localhost:3001',
-        'http://localhost:3002',
-        'http://localhost:3003',
-        'http://127.0.0.1:3000',
-        'http://127.0.0.1:3001',
-        'http://127.0.0.1:3002',
-        'http://127.0.0.1:3003',
         'https://dashboard-react-main.netlify.app',
-        env('FRONTEND_URL', 'https://dashboard-react-main.netlify.app'),
+        'http://localhost:3000',
+        'http://127.0.0.1:3000',
     ],
+
     'allowed_origins_patterns' => [],
+
     'allowed_headers' => ['*'],
+
     'exposed_headers' => [],
+
     'max_age' => 0,
-    'supports_credentials' => true, // ✅ enable credentials support
+
+    'supports_credentials' => true,
 ];
